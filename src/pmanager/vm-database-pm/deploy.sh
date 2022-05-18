78a426beb40a959d3804ed7259ed6f9feece3022
Fabio Felici <Fabio.Felici@sia.eu>
2022-05-18 10:59:20 +0000

    add diagnostic and zones on vmdb
    Signed-off-by: Fabio Felici <Fabio.Felici@sia.eu>

--- src/pmanager/vm-database-pm/deploy.sh
+++ src/pmanager/vm-database-pm/deploy.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
-if [[ "$(ls ./env)" =~ "$1" ]] && [ "$1" != "" ]; then
-    ./terraform.sh apply $1 \
-        -target=data.azurerm_resource_group.vmrg \
-        -target=data.azurerm_resource_group.rg_vnet \
-        -target=data.azurerm_virtual_network.vm_vnet \
-        -target=data.azurerm_subnet.vm_subnet \
-        -target=azurerm_network_interface.vm_nic \
-        -target=azurerm_key_vault_secret.sshkey \
-        -target=azurerm_linux_virtual_machine.vm
+CMD=$1
+ENV=$2
 
-    if (( $? == 0 )); then
-        ./terraform.sh apply $1
+if [[ "apply refresh plan" =~ "$CMD" ]]; then
+    if [[ "$(ls ./env)" =~ "$ENV" ]] && [ "$ENV" != "" ]; then
+        ./terraform.sh $CMD $ENV \
+            -target=data.azurerm_resource_group.vmrg \
+            -target=data.azurerm_resource_group.rg_vnet \
+            -target=data.azurerm_virtual_network.vm_vnet \
+            -target=data.azurerm_subnet.vm_subnet \
+            -target=azurerm_network_interface.vm_nic \
+            -target=azurerm_key_vault_secret.sshkey \
+            -target=azurerm_linux_virtual_machine.vm
+
+        if (( $? == 0 )); then
+            ./terraform.sh $CMD $ENV
+        fi
+    else
+        echo "Please select env from:\n$( ls -1 ./env )"
     fi
 else
-    echo "Please select env from:\n$( ls -1 ./env )"
+    echo "Action not allowed."
+    exit 1
 fi
